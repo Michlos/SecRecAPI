16c06c0e1ee1bfc946ff079a2e6757f9a71de41a
Michlos <michlos@live.com>
2023-08-05 04:49:08 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/ReceiveController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using SecRecAPI.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SecRecAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReceiveController : ControllerBase
+    {
+        // GET: api/<ReceiveController>
+        [HttpGet]
+        public IEnumerable<ReceiveModel> Get()
+        {
+            List<ReceiveModel> receiveModelList = new List<ReceiveModel>();
+            
+            receiveModelList.Add(new ReceiveModel() { Id=1, MailId=1 });
+            
+            return receiveModelList;
+            
+        }
+
+        // GET api/<ReceiveController>/5
+        [HttpGet("{id}")]
+        public ReceiveModel Get(int id)
+        {
+            ReceiveModel receiveModel = new ReceiveModel() { Id = 1, MailId = 1 };
+            return receiveModel;
+        }
+
+        // POST api/<ReceiveController>
+        [HttpPost("{mailid")]
+        public void Post([FromBody] ReceiveModel receive)
+        {
+        }
+
+        // PUT api/<ReceiveController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] ReceiveModel receive)
+        {
+        }
+
+        // DELETE api/<ReceiveController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Models/ReceiveModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SecRecAPI.Models
+{
+    public class ReceiveModel
+    {
+        public int Id { get; set; }
+        public int MailId { get; set; }
+        public DateTime ReceiveDate { get; set; } = DateTime.Now;
+    }
+}
